b0eba24eba6c11afcea32696003fd3f5248e1c83
changDDAO <younch8342@naver.com>
2025-06-20 19:27:07 +0900

    feat : Auth-Service의 메인 서비스인 JWT 발급 및 검증 기능 구현 및 properties secret-key 숨기기

---
+++ src/main/java/com/changddao/auth_service/JwtUtil.java
@@ -0,0 +1,34 @@
+package com.changddao.auth_service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${JWT_SECRET_KEY}")
+    private String secret;
+    private final long expirationMs = 3600000; // 1시간
+
+    public String generateToken(String userId) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims validateToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
