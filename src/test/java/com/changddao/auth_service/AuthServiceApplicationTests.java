46858b6bf2aa9bf5f93ad5c13a496649d018097b
changDDAO <younch8342@naver.com>
2025-06-20 18:36:53 +0900

    Initial commit

---
+++ src/test/java/com/changddao/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changddao.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
