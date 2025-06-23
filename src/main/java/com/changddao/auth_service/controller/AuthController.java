ff167489081bcf405cb7685a73c72752b91f487f
changDDAO <younch8342@naver.com>
2025-06-23 22:58:53 +0900

    feat : 언더바 하이픈으로 변경 및 eureka client dependency추가

--- src/main/java/com/changddao/auth_service/controller/AuthController.java
+++ src/main/java/com/changddao/auth_service/controller/AuthController.java
@@ -2,18 +2,17 @@ package com.changddao.auth_service.controller;
 
 import com.changddao.auth_service.dto.LoginRequest;
 import com.changddao.auth_service.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
+@Slf4j
 public class AuthController {
 
     @Autowired
@@ -21,10 +20,15 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        log.info("잘탔습니다.");
         if (request.getUsername().equals("test") && request.getPassword().equals("pass")) {
             String token = jwtUtil.generateToken("testUserId");
             return ResponseEntity.ok(Map.of("token", token));
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    }
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "OK";
     }
 }
